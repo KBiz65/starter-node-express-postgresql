04788697051f7a4f2759f2b28ddc58f5b9de5670
Kevin Bisner <34324949+KBiz65@users.noreply.github.com>
2021-08-19 16:18:08 -0700

    files through CRUD with Knex

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,14 +1,34 @@
-function read(req, res, next) {
-  res.json({ data: { product_title: "some product title" } });
+const products = require("../db/fixtures/products");
+const productsService = require("./products.service");
+
+//MIDDLEWARE FUNCTIONS
+function productExists(req, res, next) {
+  productsService
+    .read(req.params.productId)
+    .then((product) => {
+      if (product) {
+        res.locals.product = product;
+        return next();
+      }
+      next({ status: 404, message: `Product cannot be found.` });
+    })
+    .catch(next);
+}
+
+//CRUDL FUNCTIONS
+function read(req, res) {
+  const { product: data } = res.locals;
+  res.json({ data });
 }
 
 function list(req, res, next) {
-  res.json({
-    data: [{ product_title: "product 1" }, { product_title: "product 2" }],
-  });
+  productsService
+    .list()
+    .then((data) => res.json({ data }))
+    .catch(next);
 }
 
 module.exports = {
-  read: [read],
+  read: [productExists, read],
   list: [list],
 };
